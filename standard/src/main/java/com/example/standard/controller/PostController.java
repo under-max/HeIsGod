7c9619c2ffc984b1ef860da31194c11205d3ac64
under-max <saigrams@naver.com>
2023-05-18 12:45:01 +0900

    자기의일은?

--- standard/src/main/java/com/example/standard/controller/PostController.java
+++ standard/src/main/java/com/example/standard/controller/PostController.java
@@ -1,4 +1,14 @@
 package com.example.standard.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping
+    public void get(){
+        System.out.println("스스로 올리고 스스로 pull request하는 습관을 기릅시다");
+        System.out.println("사장은 바지 사장입니다.");
+    }
 }
