85f2b621d3ad630e8685dc76914e84a69fa98318
swkimm <swkim7161@kakao.com>
2023-05-18 12:45:24 +0900

    java 파일 수정

--- standard/src/main/java/com/example/standard/controller/PostController.java
+++ standard/src/main/java/com/example/standard/controller/PostController.java
@@ -1,4 +1,14 @@
 package com.example.standard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/")
+    public String list() {
+        return "list";
+    }
 }
