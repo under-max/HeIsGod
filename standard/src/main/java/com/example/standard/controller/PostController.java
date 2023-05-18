ce8e068567eba5a8895e865f29cb8d38052913c7
hjy <ruhjy@naver.com>
2023-05-18 12:43:47 +0900

    ex2

--- standard/src/main/java/com/example/standard/controller/PostController.java
+++ standard/src/main/java/com/example/standard/controller/PostController.java
@@ -1,4 +1,15 @@
 package com.example.standard.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
 public class PostController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home.html";
+    }
 }
